a95454d47374c9c55d88a91502d6d75bab64dd0d
Dmytro <ddorotiuk@gmail.com>
2018-11-09 21:17:54 +0100

    Updates

---
+++ precache-manifest.69a9a5c44542169a7ac302970a92200c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb7c8d3cbf92e7f0582",
+    "url": "/dorotiukdmytro/react-my-team/static/js/runtime~main.6fb7c8d3.js"
+  },
+  {
+    "revision": "eabe4291b468cc1f070a",
+    "url": "/dorotiukdmytro/react-my-team/static/js/main.eabe4291.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/dorotiukdmytro/react-my-team/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "eabe4291b468cc1f070a",
+    "url": "/dorotiukdmytro/react-my-team/static/css/main.90a7519a.chunk.css"
+  },
+  {
+    "revision": "31bcd583a03f5be7ce994f0db520e3cf",
+    "url": "/dorotiukdmytro/react-my-team/index.html"
+  }
+];
